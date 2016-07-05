1b7ec8eed1d4b5bd9e41c7a68055fc3980ee3874
Nelson I. Gonzalez <nigm2005@gmail.com>
2016-07-05 07:36:32 -0500

    Upgrading batch file, pulling task from empirical server

--- bin/run.bat
+++ bin/run.bat
@@ -1,9 +1,15 @@
-if -%1-==-- echo Use: emp experiment-name path-to-code & exit /b
-if -%2-==-- echo Use: emp experiment-name path-to-code & exit /b
-docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock^
- -v %2:/empirical/code:ro^
- -v %EMPIRICAL_DIR%/data:/empirical/data^
- -v %EMPIRICAL_DIR%/workspaces:/empirical/workspaces^
- -e EMPIRICAL_DIR=%EMPIRICAL_DIR%^
- -e DEBUG=dataset-cache^
- empiricalci/emp %1
+@ECHO OFF
+if "%1"=="" echo Use: emp run experiment-name [path-to-code] & exit /b
+if "%1"=="run" IF NOT "%2"=="" IF NOT "%3"=="" (
+	docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock^
+ 	 -v %3:/empirical/code:ro^
+ 	 -v %EMPIRICAL_DIR%/data:/empirical/data^
+ 	 -v %EMPIRICAL_DIR%/workspaces:/empirical/workspaces^
+ 	 -e EMPIRICAL_DIR=%EMPIRICAL_DIR%^
+ 	 -e DEBUG=dataset-cache^
+ 	 empiricalci/emp %2
+)
+if "%1"=="run" IF NOT "%2"=="" (
+	docker run -t --rm^
+	empiricalci/emp pull %2
+)
